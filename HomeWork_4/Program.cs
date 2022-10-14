692ef91c1bab07f2bcb633f372e14033bd9ae67b
Vitaly <sybery@yandex>
2022-10-14 09:02:04 +0300

    MAX значение

---
+++ HomeWork_4/Program.cs
@@ -0,0 +1,20 @@
+﻿int number = 2;
+int number2 = 3;
+int number3 = 7
+
+if(number < number2)
+{
+    Console.WriteLine(number2);
+}
+if(number > number2)
+{
+    Console.WriteLine(number);
+}
+if(number2 > number3)
+{
+    Console.WriteLine(number2);
+}
+if(number2 < number3)
+{
+    Console.WriteLine(number3);
+}
