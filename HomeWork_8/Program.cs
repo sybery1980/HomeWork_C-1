b164b0640231ea14bc95ef94bf3f8dbe67e104a4
Vitaly <sybery@yandex>
2022-10-15 11:00:04 +0300

    четные числа

---
+++ HomeWork_8/Program.cs
@@ -0,0 +1,12 @@
+﻿
+    
+    
+ int N = Convert.ToInt32(Console.ReadLine());
+ int start = 2;
+
+    while (start <= N)
+        {
+            Console.WriteLine(start);
+            start = start + 2;
+        }
+    
