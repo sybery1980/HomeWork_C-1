1ad29b89fad812002eb767558de0813b664c3be8
Vitaly <sybery@yandex>
2022-10-15 11:01:33 +0300

    Является ли число четным

---
+++ HomeWork_6/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+   string S = Console.ReadLine();
+   int N = Convert.ToInt32(S);
+   
+   if ( N % 2 == 0)
+ {
+   Console.WriteLine(" Число четное " );
+ }
+  else
+ {
+   Console.WriteLine(" Число нечетное " );
+ }
